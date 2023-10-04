a3c866c4afe36f1335c3e0be0f5a86ee10e555f5
Felix Stips <99824084+FelixStips@users.noreply.github.com>
2023-10-04 15:38:47 -0700

    On the way to sequential quiz

---
+++ gefinal/_static/javascript/hideInformation.js
@@ -0,0 +1,6 @@
+    function hideInformation() {
+        document.getElementById("explainer").style.display = "none";
+        document.getElementById("show-info-button").style.display = "block";
+        document.getElementById("hide-info-button").style.display = "none";
+    }
+
---
+++ gefinal/_static/javascript/showInformation.js
@@ -0,0 +1,5 @@
+  function showInformation() {
+        document.getElementById("explainer").style.display = "block";
+        document.getElementById("show-info-button").style.display = "none";
+        document.getElementById("hide-info-button").style.display = "block";
+    }
